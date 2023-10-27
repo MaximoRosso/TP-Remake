e63c4058afdc005d5328730a1ec590499f4ef2c1
Santiago Simaro Torre <santiagosimaro@gmail.com>
2023-10-27 17:32:11 -0300

    , ,
     mñkmkñ

--- Proyecto/main/main.cpp
+++ Proyecto/main/main.cpp
@@ -1,4 +1,15 @@
+#include <libreria.h>
+#define NOMBRE_CSV_CLIENTES "../iriClientesGYM.csv"
+#define NOMBRE_CSV_RESERVAS "../iriClasesGYM.csv"
+
 int main(int argc, char *argv[]) {
-    
+
+    ifstream archivo_datos_clientes;
+    archivo_datos_clientes.open(NOMBRE_CSV_CLIENTES);
+    ifstream archivo_datos_reservas;
+    archivo_datos_reservas.open(NOMBRE_CSV_RESERVAS);
+
+
+
     return 0;
 }
